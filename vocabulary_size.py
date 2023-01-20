47032506b35a69e11d25ad9242b904ec3d6292bd
Zhenyu-Zhao98 <90524503+Zhenyu-Zhao98@users.noreply.github.com>
2023-01-20 16:49:51 -0600

    Create vocabulary_size.py

---
+++ vocabulary_size.py
@@ -0,0 +1,28 @@
+import spacy
+import re
+
+
+#define how to create a list of unique words
+def unique_list(filename):
+    open_file=open(filename)
+    file_tweet =open_file.read()
+    file_tweet = re.sub(r'@(\w+)','',file_tweet)
+    file_tweet_word = re.sub(r'[0-9]*','',file_tweet)
+    file_tweet_word = re.sub(r'[/]*','',file_tweet_word)
+    file_tweet_word = re.sub(r'[-]*','',file_tweet_word)
+    file_tweet_word = re.sub(r'[.]*','',file_tweet_word)
+    file_tweet_word = re.sub(r'[,]*','',file_tweet_word)
+    words = file_tweet_word.split()
+    unique = []
+    for word in words:
+        if word not in unique:
+            unique.append(word)
+    return unique
+
+def count_unique():
+    unique_word = unique_list("tweets.txt")
+    count = len(unique_word)
+    print(f"The file tweets.txt have {count} unique words.")
+
+if __name__ == "__main__":
+    count_unique()
